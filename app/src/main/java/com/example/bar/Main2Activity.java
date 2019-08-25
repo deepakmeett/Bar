4bea81f7cf10bc59821b21e7bf50f0217b7374e5
Deepak681 <47095705+Deepak681@users.noreply.github.com>
2019-08-25 18:50:18 +0530

    Bar

---
+++ app/src/main/java/com/example/bar/Main2Activity.java
@@ -0,0 +1,68 @@
+package com.example.bar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class Main2Activity extends AppCompatActivity {
+    BottomAppBar bottomAppBar;
+    FloatingActionButton floatingActionButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        bottomAppBar = findViewById(R.id.bar);
+        floatingActionButton = findViewById(R.id.flot);
+        setSupportActionBar(bottomAppBar);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bartwo,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.mail:
+                Toast.makeText(getApplicationContext(),"Clicked on Mail",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.cloud:
+                Toast.makeText(getApplicationContext(),"Clicked on Cloud", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.delete:
+                Toast.makeText(getApplicationContext(),"Clicked on Delete", Toast.LENGTH_SHORT).show();
+                default:
+                    return super.onOptionsItemSelected(item);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/bar/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.bar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+    BottomAppBar bottomAppBar;
+    FloatingActionButton floatingActionButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN
+                , WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomAppBar = findViewById(R.id.bar);
+        floatingActionButton = findViewById(R.id.flot);
+        setSupportActionBar(bottomAppBar);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Fab is clicked", Snackbar.LENGTH_SHORT)
+                        .setAction("UNDO", null).show();
+            }
+        });
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search:
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+                return true;
+            case R.id.share:
+                Intent intent1 = new Intent(Intent.ACTION_SEND);
+                intent1.setType("text/plain");
+                String shareBody = "Your body here";
+                String shareSub = "Your Subject here";
+                intent1.putExtra(Intent.EXTRA_SUBJECT, shareSub);
+                intent1.putExtra(Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(intent1, "Share Using"));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+}
